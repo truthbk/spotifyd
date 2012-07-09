02770451c1a2569b4172bcc4107c79bc72f2978e
Jaime Fullaondo <truthbk@gmail.com>
2012-07-09 17:27:39 +0200

    Moved some code into headers. Added some C headers for spotify audio playback. Removing tags file, no need to track it.

---
+++ include/lockable.h
@@ -0,0 +1,39 @@
+#ifndef _LOCKABLE_H
+#define _LOCKABLE_H
+
+#include <cstdlib>
+
+//are extern "C" braces needed?
+#include <pthread.h>
+
+//move elsewhere.
+class Lockable {
+public:
+    Lockable() {
+            //Init...
+            pthread_mutex_init(m_mutex, NULL);
+            pthread_cond_init(m_cond, NULL);
+    }
+    ~Lockable() {
+            pthread_cond_destroy(m_cond);
+            pthread_mutex_destroy(m_mutex);
+    }
+
+    void lock() {
+            pthread_mutex_unlock(&m_mutex);
+    }
+
+    void cond_signal() {
+            pthread_cond_signal(&m_cond);
+    }
+
+    void unlock() {
+            pthread_mutex_unlock(&m_mutex);
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+    pthread_cond_t m_cond;
+};
+
+#endif
