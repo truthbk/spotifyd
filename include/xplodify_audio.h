4d69f3d7b388f1749f94ac22de18164d2a957e3e
Jaime Fullaondo (truthbk) <truthbk@gmail.com>
2013-06-12 04:05:57 +0200

    Adding skeleton for what will be the C++ audio management classes. Walking away from the original C stuff.

---
+++ include/xplodify_audio.h
@@ -0,0 +1,31 @@
+#ifndef _XPLODIFY_AUDIO_H
+#ifndef _XPLODIFY_AUDIO_H
+
+#include <queue>
+#include <cstdint>
+
+#include "lockable.h"
+#include "runnable.h"
+
+struct audio_data {
+    public:
+        uint32_t channels;
+        uint32_t rate;
+        uint32_t channels;
+        std::vector<int16_t> samples;
+}
+
+//This will probably end up being a superclass
+class XplodifyAudio 
+         : public Runnable
+         , private Lockable {
+
+    protected:
+        void initialize();
+        // implemeting runnable
+        void run();
+    private:
+        std::queue<audio_data> audio_queue;
+}
+
+#endif //_XPLODIFY_AUDIO_HH
