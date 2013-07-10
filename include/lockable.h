446442cc783cfa30fdf5bdd19c04fd934f9522dd
truthbk <truthbk@gmail.com>
2013-07-10 14:47:28 +0200

    The ticking store was just overkill, going back to old model. Will have to battle the poor libspotify documentation relevant to callbacks and loading.

--- include/lockable.h
+++ include/lockable.h
@@ -8,40 +8,40 @@
 
 //move elsewhere.
 class Lockable {
-public:
-    Lockable() {
+    public:
+        Lockable() {
             //Init...
             pthread_mutex_init(&m_mutex, NULL);
             pthread_cond_init(&m_cond, NULL);
-    }
-    ~Lockable() {
+        }
+        ~Lockable() {
             pthread_cond_destroy(&m_cond);
             pthread_mutex_destroy(&m_mutex);
-    }
+        }
 
-    void lock() {
+        void lock() {
             pthread_mutex_unlock(&m_mutex);
-    }
+        }
 
-    void cond_signal() {
-            pthread_cond_signal(&m_cond);
-    }
+        int cond_signal() {
+            return pthread_cond_signal(&m_cond);
+        }
 
-    void cond_wait() {
-            pthread_cond_wait(&m_cond, &m_mutex);
-    }
+        int cond_wait() {
+            return pthread_cond_wait(&m_cond, &m_mutex);
+        }
 
-    void cond_timedwait(struct timespec * ts) {
-            pthread_cond_timedwait(&m_cond, &m_mutex, ts);
-    }
+        int cond_timedwait(struct timespec * ts) {
+            return pthread_cond_timedwait(&m_cond, &m_mutex, ts);
+        }
 
-    void unlock() {
+        void unlock() {
             pthread_mutex_unlock(&m_mutex);
-    }
+        }
 
-private:
-    pthread_mutex_t m_mutex;
-    pthread_cond_t m_cond;
+    private:
+        pthread_mutex_t m_mutex;
+        pthread_cond_t m_cond;
 };
 
 #endif
