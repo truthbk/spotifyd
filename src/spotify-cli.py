9e2e484014f08131a5f3673872928253ded741ad
Christian Stefanescu <st.chris@gmail.com>
2013-07-24 21:20:37 +0200

    Fixed most Flake8 violations.

--- src/spotify-cli.py
+++ src/spotify-cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# -*- coding: latin-1 -*- 
+# -*- coding: latin-1 -*-
 
 import sys
 import time
@@ -8,35 +8,30 @@ import getopt
 import os.path
 
 #temporary hack, we'll be moving to virtualenv.
-sys.path.insert(1,'./gen-py')
+sys.path.insert(1, './gen-py')
 
 from spotify import Spotify
 from spotify.ttypes import *
 from spotify.constants import *
 
 import thrift
-from thrift import Thrift
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-import curses
-from curses import panel
-
 import urwid
-
-import signal
-import getpass
 
 import logging
 
 XPLODIFYD_PORT = 9090
 VERSION = "0.1"
 
-logging.basicConfig(filename='debug.log',level=logging.DEBUG)
+logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+
 
 def signal_handler(signal, frame):
     sys.exit(0)
+
 
 class spclient(object):
 
@@ -60,7 +55,6 @@ class spclient(object):
             print "Couldn't connect to server on {}:{}".format(host, port)
             sys.exit(1)
 
-
         #status
         self._success = True
 
@@ -74,11 +68,11 @@ class spclient(object):
         self._currentplaylist = None
 
     def login(self, username, password):
-        success=False
+        success = False
         try:
-            credentials = SpotifyCredential( username, password )
+            credentials = SpotifyCredential(username, password)
             self._credentials = self._client.loginSession(credentials)
-            success=True #refine this.
+            success = True  # refine this.
         except Exception, e:
             logging.debug("Exception: %s", e)
             return False
@@ -106,7 +100,7 @@ class spclient(object):
             return ret
 
         try:
-            credentials = SpotifyCredential( username )
+            credentials = SpotifyCredential(username)
             ret = self._client.isLoggedIn(credentials)
 
         except Exception, e:
@@ -131,7 +125,10 @@ class spclient(object):
         tracks = None
         try:
             """ pls will be a set with the playlists """
-            tracks = self._client.getPlaylistByName(self._credentials, playlist)
+            tracks = self._client.getPlaylistByName(
+                self._credentials,
+                playlist
+            )
 
         except Exception, e:
             logging.debug("Exception: %s", e)
@@ -193,38 +190,40 @@ class spclient(object):
             self._success = False
             return None
 
-
     def spot_getcurrent(self):
         return None
 
 
 class XplodifyElement(urwid.Button):
     def __init__(self, el_id, el_name, callback=None, userdata=None):
-        self.el_id = el_id 
+        self.el_id = el_id
         self.el_name = el_name
-        super(XplodifyElement, self).__init__(self.el_name, on_press=callback, user_data=userdata)
+        super(XplodifyElement, self).__init__(
+            self.el_name,
+            on_press=callback,
+            user_data=userdata
+        )
 
 
 class XplodifyApp(urwid.Frame):
     palette = [
-            ('body','default', 'default'),
-            ('reversed','light gray', 'black', 'bold'),
-            ('playback','light red', 'dark gray', 'bold'),
-            ('foot','black', 'light gray', 'bold'),
-            ('head','black', 'light gray', 'bold'),
-            ('key','light cyan', 'dark blue', 'underline'),
-            ]
-
+        ('body', 'default', 'default'),
+        ('reversed', 'light gray', 'black', 'bold'),
+        ('playback', 'light red', 'dark gray', 'bold'),
+        ('foot', 'black', 'light gray', 'bold'),
+        ('head', 'black', 'light gray', 'bold'),
+        ('key', 'light cyan', 'dark blue', 'underline'),
+    ]
 
     footer_text = ('foot', [
-        "Xpldofiy Client "+ VERSION +" -     " ,
+        "Xpldofiy Client " + VERSION + " -     ",
         ('key', "F4"), " login   ",
         ('key', "F5"), " |<   ",
         ('key', "F7"), " |> / ||   ",
         ('key', "F8"), " >|   ",
         ('key', "F9"), " logout  ",
         ('key', "F10"), " quit ",
-        ])
+    ])
 
     def __init__(self, host='localhost', port=XPLODIFYD_PORT):
         self.logged = False
@@ -244,46 +243,73 @@ class XplodifyApp(urwid.Frame):
         self.footer = urwid.AttrWrap(urwid.Text(self.footer_text), "foot")
 
         self.widgets = [
-                self.plpane,
-                self.trackpane
-                ]
+            self.plpane,
+            self.trackpane
+        ]
 
-        email = urwid.Edit(u'Username:  ', u"", allow_tab=False, multiline=False)
-        passwd = urwid.Edit(u'Password:  ', u"", allow_tab=False, multiline=False, mask=u"*" )
+        email = urwid.Edit(
+            u'Username:  ',
+            u"",
+            allow_tab=False,
+            multiline=False
+        )
+        passwd = urwid.Edit(
+            u'Password:  ',
+            u"",
+            allow_tab=False,
+            multiline=False,
+            mask=u"*"
+        )
         logbutton = urwid.Button(u'Login')
         urwid.connect_signal(logbutton, 'click', self.login)
 
-        self.mainview = urwid.Columns(self.widgets, dividechars=1, focus_column=0)
-        self.loginview = urwid.Filler(urwid.Pile([email, passwd,
-                urwid.AttrMap(logbutton, None, focus_map='reversed')]))
-        self.overlay = urwid.Overlay(self.loginview, self.mainview,
-                align='center', width=('relative', 30),
-                valign='middle', height=('relative', 30),
-                min_width=30, min_height=6)
-        super(XplodifyApp, self).__init__(urwid.AttrWrap(self.mainview, 'body'), footer=self.footer, header=self.header)
+        self.mainview = urwid.Columns(
+            self.widgets,
+            dividechars=1,
+            focus_column=0
+        )
+        self.loginview = urwid.Filler(urwid.Pile([
+            email,
+            passwd,
+            urwid.AttrMap(logbutton, None, focus_map='reversed')
+        ]))
+        self.overlay = urwid.Overlay(
+            self.loginview,
+            self.mainview,
+            align='center', width=('relative', 30),
+            valign='middle', height=('relative', 30),
+            min_width=30, min_height=6)
+        super(XplodifyApp, self).__init__(
+            urwid.AttrWrap(self.mainview, 'body'),
+            footer=self.footer,
+            header=self.header
+        )
 
         urwid.set_encoding("ISO-8859-*")
-        self.loop = urwid.MainLoop(self, self.palette,
-             unhandled_input=self.unhandled_keypress)
+        self.loop = urwid.MainLoop(
+            self,
+            self.palette,
+            unhandled_input=self.unhandled_keypress
+        )
 
     def main(self):
         self.loop.run()
 
     def login_overlay(self):
-        display = None
         if self.logged:
             pass
         else:
             self.body = self.overlay
 
     def login(self, key):
-        username = self.loginview.original_widget.widget_list[0].get_edit_text()
+        username = self.loginview.original_widget.\
+            widget_list[0].get_edit_text()
         passwd = self.loginview.original_widget.widget_list[1].get_edit_text()
         if not self.logged:
             self.logged = self.spoticlient.login(username, passwd)
         time.sleep(10)
         if self.logged:
-            self.header.original_widget.set_text(u"Logged in as "+username) 
+            self.header.original_widget.set_text(u"Logged in as "+username)
             self.get_playlists()
             self.mainview.set_focus_column(0)
         self.body = self.mainview
@@ -291,14 +317,13 @@ class XplodifyApp(urwid.Frame):
     def logout(self):
         if self.logged:
             self.spoticlient.logout()
-            
+
             self._active_pl = None
             self._active_pl_button = None
 
             self.clear_pl_panel()
             self.clear_track_panel()
             self.header.original_widget.set_text(u"Not Logged in.")
-
 
     def get_playlists(self):
         try:
@@ -316,7 +341,12 @@ class XplodifyApp(urwid.Frame):
             for pl in self._playlists:
                 try:
                     self._plwalker.insert(0, urwid.AttrMap(
-                        XplodifyElement(pid, pl, callback=self.set_track_panel, userdata=pl),
+                        XplodifyElement(
+                            pid,
+                            pl,
+                            callback=self.set_track_panel,
+                            userdata=pl
+                        ),
                         None, focus_map='reversed'))
                     self.get_tracks(pl)
                     pid += 1
@@ -350,13 +380,21 @@ class XplodifyApp(urwid.Frame):
                 w.set_attr_map({None: 'reversed'})
                 self._active_pl_button = w
 
-            tid=1
             for track in self._tracks[playlist]:
                 logging.debug("Processing track: %s", track._name)
-                self._trwalker.insert(0, urwid.AttrMap(
-                        XplodifyElement(track._id, track._name+" - "+track._artist, 
-                            callback=self.playback_track, userdata=track),
-                        None, focus_map='reversed'))
+                self._trwalker.insert(
+                    0,
+                    urwid.AttrMap(
+                        XplodifyElement(
+                            track._id,
+                            track._name + " - " + track._artist,
+                            callback=self.playback_track,
+                            userdata=track
+                        ),
+                        None,
+                        focus_map='reversed'
+                    )
+                )
 
     def clear_pl_panel(self):
         while self._plwalker:
@@ -401,8 +439,7 @@ class XplodifyApp(urwid.Frame):
         except Exception, e:
             logging.debug("Exception: %s", e)
 
-
-    def set_playlist(self, button, playlist): 
+    def set_playlist(self, button, playlist):
         return
 
     def quit(self):
@@ -417,7 +454,7 @@ class XplodifyApp(urwid.Frame):
         elif k == "f6":
             raise urwid.ExitMainLoop()
         elif k == "f7":
-            self.toggle_playback();
+            self.toggle_playback()
         elif k == "f8":
             raise urwid.ExitMainLoop()
         elif k == "f9":
@@ -426,27 +463,31 @@ class XplodifyApp(urwid.Frame):
             self.logout()
             raise urwid.ExitMainLoop()
         elif k == "tab":
-            self.mainview.focus_position = (self.mainview.focus_position + 1) % 2
+            self.mainview.focus_position = (
+                self.mainview.focus_position + 1
+            ) % 2
         else:
             return
 
         return True
 
+
 def usage():
-    BOLD_START="\033[1m"
-    BOLD_END="\033[0m"
+    BOLD_START = "\033[1m"
+    BOLD_END = "\033[0m"
     print "usage: "+os.path.basename(__file__)+" [OPTIONS]"
     print "\nOPTIONS:"
     print "\t"+BOLD_START+"-h"+BOLD_END+"\n\t\tDisplay this help message."
     print "\t"+BOLD_START+"-s host,--server=host"+BOLD_END+"\n\t\tSpecify service hostname."
     print "\t"+BOLD_START+"-p PORT,--port=PORT"+BOLD_END+"\n\t\tSpecify service port."
 
+
 def main(argv):
-    server='localhost'
-    port=XPLODIFYD_PORT
+    server = 'localhost'
+    port = XPLODIFYD_PORT
 
     try:
-        opts, args = getopt.getopt(argv,"hs:p:", ["server=","port="])
+        opts, args = getopt.getopt(argv, "hs:p:", ["server=", "port="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -463,5 +504,5 @@ def main(argv):
     XplodifyApp(host=server, port=port).main()
 
 
-if  __name__ =='__main__':
+if __name__ == '__main__':
     main(sys.argv[1:])
