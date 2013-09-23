e72212a0e51c475990a0f84d4fcaab73e77655dd
Jaime Fullaondo (truthbk) <truthbk@gmail.com>
2013-09-24 01:31:35 +0200

    Adding constructor. Also, m_cached is protected, not private.

--- include/cacheable.h
+++ include/cacheable.h
@@ -3,11 +3,14 @@
 
 class Cacheable {
     public:
+        Cacheable() 
+            : m_cached(false) {
+        }
         virtual void cache(void) = 0;
         virtual void uncache(void) = 0;
         virtual bool is_cached(void) = 0;
-    private:
-        bool cached;
+    protected:
+        bool m_cached;
 };
 
 #endif /* _CACHEABLE_H*/
