038f0bbaf5e05ed2155e1977bb891be66b7b2599
Jaime Fullaondo (truthbk) <truthbk@gmail.com>
2013-11-14 01:06:39 +0100

    Adding thrift service server handler skeleton.

---
+++ include/spotify_ipc_srv.h
@@ -0,0 +1,14 @@
+#ifndef _SPOTIFY_CUST_HH
+#define _SPOTIFY_CUST_HH
+
+#include "SpotifyIPC.h"
+
+class XplodifyIPCServer
+    : virtual public SpotifyIPCIf
+{
+    public:
+        XplodifyIPCServer();
+
+}
+
+#endif //_SPOTIFY_CUST_HH
