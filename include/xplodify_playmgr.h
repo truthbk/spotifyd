55d0af2f5704b84fb6b4994225dcbf4bf4eda470
Jaime Fullaondo (truthbk) <truthbk@gmail.com>
2014-01-18 20:11:26 +0100

    Some changes to xplodify playback manager header file.

--- include/xplodify_playmgr.h
+++ include/xplodify_playmgr.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <map>
+#include <string>
 
 #include <boost/shared_ptr.hpp>
 
@@ -14,7 +15,8 @@ class XplodifyPlaybackManager
     : private Lockable
 {
     public:
-        XplodifyPlaybackManager(uint8_t n_procs, uint32_t base_port);
+        XplodifyPlaybackManager(
+                std::string host, uint32_t base_port, uint8_t n_procs);
         bool switch_roles(void);
         bool login(std::string user, std::string passwd);
         bool is_logged_in(std::string user);
@@ -29,18 +31,33 @@ class XplodifyPlaybackManager
         void stop(void);
 
     private:
-        const uint8_t n_procs;
-        const uint32_t base_port;
+        const uint32_t m_host;
+        const uint32_t m_base_port;
+        const uint8_t m_n_procs;
         uint8_t master;
 
         struct XplodifyClient {
-            public:
-                boost::shared_ptr<TSocket> socket;
-                boost::shared_ptr<TBufferedTransport> transport;
-                boost::shared_ptr<TBinaryProtocol> protocol;
+            std::string _host;
+            uint32_t _port;
 
-                SpotifyClient client;
-                bool master;
+            boost::shared_ptr<TSocket> _socket;
+            boost::shared_ptr<TBufferedTransport> _transport;
+            boost::shared_ptr<TBinaryProtocol> _protocol;
+
+            SpotifyClient _client;
+
+            bool _master;
+
+            XplodifyClient(std::string host, uint32_t port)
+                : _host(host)
+                , _port(port)
+                , _socket(new TSocket(_host, _port))
+                , _transport(new TBufferedTransport(_socket))
+                , _protocol(new TBinaryProtocol(_transport))
+                , _client(_protocol)
+                , _master(false)
+            {
+            }
         }
 
         std::map<uint32_t,boost::shared_ptr<XplodifyClient> > clients;
